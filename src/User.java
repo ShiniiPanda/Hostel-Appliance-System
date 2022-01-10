6904bd1c3b1fd9c064bb1ac89bd4e4498feaf9f2
ShiniiPanda <celisiagaming@gmail.com>
2022-01-10 22:21:57 +0200

    Removed old getters and setters

--- src/User.java
+++ src/User.java
@@ -16,47 +16,6 @@ abstract public class User extends Person {
         this.password = password;
     }
 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
-    }
-
     public String getPassword() {
         return password;
     }

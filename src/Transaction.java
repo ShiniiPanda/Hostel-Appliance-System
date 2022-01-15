e047f039f2031a997402087d580d6e117c9606ef
ShiniiPanda <celisiagaming@gmail.com>
2022-01-15 02:51:53 +0200

    Added private Appointment variable to Transaction class to create link

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,17 +2,20 @@ public class Transaction {
 
     private Customer sender;
     private String invoiceID;
+    private Appointment appointment;
     private float amount;
 
     public Transaction() {
         this.invoiceID = "000";
         this.sender = new Customer();
+        this.appointment = new Appointment();
         this.amount = 0;
     }
 
-    public Transaction(String invoiceID, Customer sender, float amount) {
+    public Transaction(String invoiceID, Customer sender, Appointment appointment, float amount) {
         this.invoiceID = invoiceID;
         this.sender = sender;
+        this.appointment = appointment;
         this.amount = amount;
     }
 
@@ -30,6 +33,14 @@ public class Transaction {
 
     public void setInvoiceID(String invoiceID) {
         this.invoiceID = invoiceID;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
     }
 
     public float getAmount() {

05ca2b414891db230d55c99c7e02d03f72d2c559
ShiniiPanda <celisiagaming@gmail.com>
2022-01-18 15:50:17 +0200

    Added a second comment to Feedback.java

--- src/Feedback.java
+++ src/Feedback.java
@@ -12,6 +12,7 @@ public class Feedback {
     }
 
     //This is a constructor for the feedback class
+    //Second Comment
     public Feedback(Customer customer, String content, String date, HomeAppliance appliance) {
         this.customer = customer;
         this.content = content;

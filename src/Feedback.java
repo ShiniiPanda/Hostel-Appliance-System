c5b3ab9fa623e6a4fd013c9a8da78a64f4ac4088
ShiniiPanda <celisiagaming@gmail.com>
2022-02-09 23:51:30 +0300

    Feedback object now includes rating, appointment ID. Added toTextFormat.

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,22 +1,33 @@
-public class Feedback {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Feedback implements TextStored {
 
     private Customer customer;
+    private Appointment appointment;
     private String content, date;
+    private int rating;
     private HomeAppliance appliance;
 
     public Feedback(){
         this.customer = new Customer();
-        this.content = "null";
-        this.date = "00/00/0000";
         this.appliance = new HomeAppliance();
+        this.appointment = new Appointment();
+        this.date = "0000/00/00";
+        this.rating = 5;
+        this.content = "No Message";
     }
 
     //This is a constructor for the feedback class
-    public Feedback(Customer customer, String content, String date, HomeAppliance appliance) {
+    public Feedback(Customer customer, HomeAppliance appliance, Appointment appointment, String date, int rating, String content ) {
         this.customer = customer;
-        this.content = content;
-        this.date = date;
         this.appliance = appliance;
+        this.appointment = appointment;
+        this.date = date;
+        this.rating = rating;
+        this.content = content;
     }
 
     public Customer getCustomer() {
@@ -35,6 +46,22 @@ public class Feedback {
         this.date = date;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+
     public String getContent() {
         return content;
     }
@@ -49,5 +76,24 @@ public class Feedback {
 
     public void setAppliance(HomeAppliance appliance) {
         this.appliance = appliance;
+    }
+
+    public static void addNewFeedback(Feedback f){
+        String record = f.toTextFormat() + "\n";
+        try {
+            FileWriter fileWriter = new FileWriter("./TextFiles/Feedback.txt", true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            writer.write(record);
+            writer.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage() + "\nError writing feedback");
+        }
+    }
+
+    @Override
+    public String toTextFormat() {
+        return this.customer.getId() + "//" + this.appliance.getId() + "//" +
+                this.appointment.getId() + "//" + this.date + "//" + this.rating + "//" + this.content;
     }
 }
